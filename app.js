2534d651b35ce530b7a8d80abfeb756c25937e0b
Benny <johansson.benny@gmail.com>
2020-06-03 11:23:05 +0200

    tesgt

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
  
-app.get('/', (req, res) => {
+app.get("/", (req, res, next) => {
 
     res.send("Welcome to ekosim");
 
 });
 
 const port = process.env.port || 3000;
-app.listen(port, () => {
+app.listen(port, function() {
     console.log("Node server startedd");
 });
